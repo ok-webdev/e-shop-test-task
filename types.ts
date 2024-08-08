91b412cd64debee495f3016514e5a8e68367777d
Oleg Kokoshinskii <oleg.kokoshinskii@gmail.com>
2024-08-08 15:39:40 +0300

    create item interface

---
+++ types.ts
@@ -0,0 +1,12 @@
+export interface IItem {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  rating: {
+    count: number;
+    rate: number;
+  };
+  title: string;
+}
