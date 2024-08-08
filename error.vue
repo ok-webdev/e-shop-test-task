c7a337f1d78364020a54640b1f6135664c18009c
Oleg Kokoshinskii <oleg.kokoshinskii@gmail.com>
2024-08-08 15:57:51 +0300

    replace link with button

--- error.vue
+++ error.vue
@@ -2,13 +2,17 @@
   const { error } = defineProps<{
     error: { statusCode: number; message: string };
   }>();
+
+  const handleError = () => {
+    clearError({ redirect: '/' });
+  };
 </script>
 <template>
   <div class="error">
     <h1>Oops...</h1>
     <h2>{{ error.statusCode }}</h2>
     <h3>{{ error.message }}</h3>
-    <NuxtLink to="/">Home page</NuxtLink>
+    <base-button @click="handleError">Home?</base-button>
   </div>
 </template>
 <style scoped>
