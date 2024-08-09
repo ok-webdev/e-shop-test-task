f04391c3bd3553653aeebcf20330b15d709d5074
Oleg Kokoshinskii <oleg.kokoshinskii@gmail.com>
2024-08-09 23:15:56 +0300

    refactor + add filter

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-  import type { IItem } from '@/types';
+  import { ref, watch } from 'vue';
+  import type { IItem, ICartItem } from '@/types';
+  import { useAddToCart } from '@/hooks/addToCart';
 
   useHead({
     title: 'Another test task | Home',
@@ -8,18 +10,59 @@
   const { data: items } = await useFetch<IItem[]>(
     'https://fakestoreapi.com/products'
   );
+
+  let filteredItems = ref(items);
+
+  const { data: categories } = await useFetch<string[]>(
+    'https://fakestoreapi.com/products/categories'
+  );
+
+  const filter = ref('all');
+
+  const filterHandler = (event: Event) => {
+    const filterValue = (event.target as HTMLTextAreaElement).value;
+    console.log(filterValue);
+  };
+
+  watch(filter, async (newValue, oldValue) => {
+    if (newValue !== oldValue) {
+      const { data } = await useFetch<IItem[]>(
+        `https://fakestoreapi.com/products/category/${newValue}`
+      );
+      filteredItems.value = data.value;
+    }
+  });
+
+  const { addToCart } = useAddToCart();
 </script>
 
 <template>
-  <div class="cards container">
-    <item-card
-      v-for="item in items"
-      :key="item.id"
-      :id="item.id"
-      :title="item.title"
-      :price="item.price"
-      :src="item.image"
-    />
+  <div class="container">
+    <select class="filter" v-model="filter" @change="filterHandler">
+      <option value="all">all</option>
+      <option v-for="category in categories" :key="category" :value="category">
+        {{ category }}
+      </option>
+    </select>
+    <div class="cards">
+      <item-card
+        v-for="item in filteredItems"
+        :key="item.id"
+        :id="item.id"
+        :title="item.title"
+        :price="item.price"
+        :src="item.image"
+        @add="
+          addToCart({
+            id: item.id,
+            src: item.image,
+            title: item.title,
+            price: item.price,
+            quantity: 1,
+          })
+        "
+      />
+    </div>
   </div>
 </template>
 
@@ -29,5 +72,13 @@
     justify-content: center;
     flex-wrap: wrap;
     gap: 20px;
+    margin-top: 20px;
+  }
+  .filter {
+    display: block;
+    padding: 10px;
+    margin: 0 auto;
+    border: 1px solid #ccc;
+    border-radius: 5px;
   }
 </style>
