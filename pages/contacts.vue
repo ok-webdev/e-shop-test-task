ac38345628a62f9dfa8055b10c7d6d229bc64516
Oleg Kokoshinskii <oleg.kokoshinskii@gmail.com>
2024-08-09 23:14:31 +0300

    add ClientOnly to contacts

--- pages/contacts.vue
+++ pages/contacts.vue
@@ -4,24 +4,30 @@
   });
 </script>
 <template>
-  <div class="container">
-    <h1>Contacts</h1>
-    <ul>
-      <li>
-        <a href="https://maps.app.goo.gl/AQHZJxjYPdXHVjng8" target="_blank"
-          >Ulitsa Radishcheva, 39, 28, Sankt-Peterburg, 191123</a
-        >
-      </li>
-      <li>
-        <a href="mailto:info@kefirium.ru" target="_top">info@kefirium.ru</a>
-      </li>
-    </ul>
-  </div>
+  <ClientOnly>
+    <div class="container">
+      <h1>Contacts</h1>
+      <ul>
+        <li>
+          <a href="https://maps.app.goo.gl/AQHZJxjYPdXHVjng8" target="_blank"
+            >Ulitsa Radishcheva, 39, 28, Sankt-Peterburg, 191123</a
+          >
+        </li>
+        <li>
+          <a href="mailto:info@kefirium.ru" target="_top">info@kefirium.ru</a>
+        </li>
+      </ul>
+    </div>
+  </ClientOnly>
 </template>
 
 <style scoped>
+  ul {
+    list-style-type: none;
+    margin-top: 20px;
+  }
   li {
     max-width: fit-content;
-    margin-bottom: 10px;
+    margin-bottom: 15px;
   }
 </style>
