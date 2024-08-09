80cac725d283cd0774b483c9891d74177009b44b
Oleg Kokoshinskii <oleg.kokoshinskii@gmail.com>
2024-08-09 12:04:42 +0300

    add target argument to links

--- pages/contacts.vue
+++ pages/contacts.vue
@@ -4,17 +4,21 @@
   });
 </script>
 <template>
-  <div class="container">
-    <h1>Contacts</h1>
-    <ul>
-      <li>
-        <a href="https://maps.app.goo.gl/AQHZJxjYPdXHVjng8"
-          >Ulitsa Radishcheva, 39, 28, Sankt-Peterburg, 191123</a
-        >
-      </li>
-      <li><a href="mailto:info@kefirium.ru">info@kefirium.ru</a></li>
-    </ul>
-  </div>
+  <no-ssr>
+    <div class="container">
+      <h1>Contacts</h1>
+      <ul>
+        <li>
+          <a href="https://maps.app.goo.gl/AQHZJxjYPdXHVjng8" target="_blank"
+            >Ulitsa Radishcheva, 39, 28, Sankt-Peterburg, 191123</a
+          >
+        </li>
+        <li>
+          <a href="mailto:info@kefirium.ru" target="_top">info@kefirium.ru</a>
+        </li>
+      </ul>
+    </div>
+  </no-ssr>
 </template>
 
 <style scoped>
