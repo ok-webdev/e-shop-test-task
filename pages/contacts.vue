6217a5c446c8cfc976ab8cab55b38581a1381c3e
Oleg Kokoshinskii <oleg.kokoshinskii@gmail.com>
2024-08-09 11:59:08 +0300

    add contacts

--- pages/contacts.vue
+++ pages/contacts.vue
@@ -4,5 +4,22 @@
   });
 </script>
 <template>
-  <div class="container">Contacts</div>
+  <div class="container">
+    <h1>Contacts</h1>
+    <ul>
+      <li>
+        <a href="https://maps.app.goo.gl/AQHZJxjYPdXHVjng8"
+          >Ulitsa Radishcheva, 39, 28, Sankt-Peterburg, 191123</a
+        >
+      </li>
+      <li><a href="mailto:info@kefirium.ru">info@kefirium.ru</a></li>
+    </ul>
+  </div>
 </template>
+
+<style scoped>
+  li {
+    max-width: fit-content;
+    margin-bottom: 10px;
+  }
+</style>
