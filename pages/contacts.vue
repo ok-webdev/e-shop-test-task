9c4004fc5d396d28d9527c1b7dfc28e731a1b2a6
Oleg Kokoshinskii <oleg.kokoshinskii@gmail.com>
2024-08-09 12:19:18 +0300

    remove no-ssr

--- pages/contacts.vue
+++ pages/contacts.vue
@@ -4,21 +4,19 @@
   });
 </script>
 <template>
-  <no-ssr>
-    <div class="container">
-      <h1>Contacts</h1>
-      <ul>
-        <li>
-          <a href="https://maps.app.goo.gl/AQHZJxjYPdXHVjng8" target="_blank"
-            >Ulitsa Radishcheva, 39, 28, Sankt-Peterburg, 191123</a
-          >
-        </li>
-        <li>
-          <a href="mailto:info@kefirium.ru" target="_top">info@kefirium.ru</a>
-        </li>
-      </ul>
-    </div>
-  </no-ssr>
+  <div class="container">
+    <h1>Contacts</h1>
+    <ul>
+      <li>
+        <a href="https://maps.app.goo.gl/AQHZJxjYPdXHVjng8" target="_blank"
+          >Ulitsa Radishcheva, 39, 28, Sankt-Peterburg, 191123</a
+        >
+      </li>
+      <li>
+        <a href="mailto:info@kefirium.ru" target="_top">info@kefirium.ru</a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
